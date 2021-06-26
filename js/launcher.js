e1c469000c2c361b1cb600a050ace96a7b87bfa2
vyshvydu <79267593+vyshvydu@users.noreply.github.com>
2021-06-26 12:58:42 +0530

    Add files via upload

---
+++ js/launcher.js
@@ -0,0 +1,36 @@
+class launcher{
+	constructor(body,anchor)
+	{
+		
+		var options={ bodyA:body,			 
+			pointB:anchor, 
+			stiffness:0.14, 
+			length:1
+			
+		}
+		
+		this.bodyA=body
+		this.pointB=anchor
+		this.launcher=Constraint.create(options)
+		World.add(world,this.launcher)
+	}
+
+	attach(body){
+		this.launcher.bodyA=body;
+	}
+
+	fly(){
+		this.launcher.bodyA=null;
+	}
+
+	display()
+	{
+		if(this.launcher.bodyA)
+		{
+			var pointA=this.bodyA.position;
+			var pointB=this.pointB
+
+			strokeWeight(2);		
+			line(pointA.x,pointA.y,pointB.x,pointB.y);
+		}
+	}}
